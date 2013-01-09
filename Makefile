cc3355869c5cd5b62af493013d534f5ea38c23e5
Filippos Giannakos <philipgian@grnet.gr>
2013-01-09 19:27:33 +0200

    Fix installation for archipelago packages

--- Makefile
+++ Makefile
@@ -5,11 +5,14 @@ default: xseg
 build: xseg
 
 xseg:
-	make -C xseg XSEG_DOMAIN_TARGETS="user"
+	make -C xseg XSEG_DOMAIN_TARGETS="user kernel"
 
 clean:
 	make -C xseg clean
 	rm ./config.mk
+
+install:
+	make -C xseg install
 
 distclean:
 	make -C xseg distclean
--- make_pkg.sh
+++ make_pkg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
 
-./make_install_file.sh > debian/archipelago-kernel-dkms.install 
-git add debian/archipelago-kernel-dkms.install
+#./make_install_file.sh > debian/archipelago-kernel-dkms.install 
+#git add debian/archipelago-kernel-dkms.install
 export LD_LIBRARY_PATH=$PWD/xseg/lib:$LD_LIBRARY_PATH
 git-buildpackage --git-upstream-branch=$1 \
 		 --git-debian-branch=$2 \
