c2a494c9016ee906df3ee0d2bf96a3bedfdf6802
Filippos Giannakos <philipgian@grnet.gr>
2014-02-14 17:37:29 +0200

    Strip xseg stuff

--- xseg/base.mk
+++ xseg/base.mk
@@ -34,14 +34,6 @@
 
 # Setup for xseg Makefiles.
 
-ifndef TARGET
-TARGET:=$(shell basename $(shell pwd))
-endif
-
-export CC=gcc
-ifndef MOPTS
-export MOPTS=
-endif
 ifndef COPTS
 export COPTS=-O2 -g -finline-functions $(MOPTS) $(DEBUG)
 endif
@@ -49,68 +41,9 @@ ifndef CSTD
 export CSTD=-std=gnu99 -pedantic
 endif
 
-export TOPDIR=$(shell dirname $(CURDIR))
-ifeq (,$(VERSION))
-export VERSION=$(shell cat $(TOPDIR)/version)
-endif
-
-ifeq (,$(DESTDIR))
-export DESTDIR=/
-endif
-
-ifeq (,$(KVER))
-export KVER=$(shell uname -r)
-endif
-
-
 bindir=/usr/bin/
-libdir=/usr/lib/
 pythondir=/usr/lib/python2.7/
-moduledir=/lib/modules/$(KVER)/extra/
-srcdir=/usr/src/archipelago-modules-dkms-$(VERSION)/xseg/
 ganetidir=/usr/share/ganeti/extstorage/archipelago/
 confdir=/etc/archipelago/
 ganetihooksdir=/etc/ganeti/hooks/
 
-INC=-I$(BASE)
-INC+=-I$(BASE)/peers/$(TARGET)
-INC+=-I$(BASE)/sys/$(TARGET)
-INC+=-I$(BASE)/drivers/$(TARGET)
-export INC
-
-export LIB=$(BASE)/lib/$(TARGET)
-export CFLAGS=-Wall $(COPTS) $(CSTD)
-
-#ifeq (,$(XSEG_HOME))
-#export XSEG_HOME=$(shell ${XSEG_HOME})
-#endif
-
-ifeq (,$(XSEG_HOME))
-export XSEG_HOME=$(CURDIR)
-endif
-
-CONFIG=./config.mk
-
-#default:
-
-#.PHONY: clean-config
-
-#clean: clean-config
-
-#clean-config:
-#	rm -f $(CONFIG)
-
-ifndef BASE
-exists=$(shell [ -f "$(CONFIG)" ] && echo exists)
-ifeq (exists,$(exists))
-include $(CONFIG)
-else
-$(shell $(XSEG_HOME)/envsetup show | sed -e 's/"//g' > "$(CONFIG)")
-include $(CONFIG)
-endif
-
-ifeq (,$(XSEG_DOMAIN_TARGETS))
-export XSEG_DOMAIN_TARGETS=$(shell $(XSEG_HOME)/tools/xseg-domain-targets | sed -e 's/^[^=]*=//;s/"//g')
-endif
-export BASE=$(XSEG_HOME)
-endif
