d138a123424d9a6348017e3aeea5c9e98deca119
Chrysostomos Nanakos <cnanakos@grnet.gr>
2014-12-23 21:36:02 +0200

    poold: Add archipelago::System::set_system function
    Signed-off-by: Chrysostomos Nanakos <cnanakos@grnet.gr>

--- src/poold.cc
+++ src/poold.cc
@@ -21,6 +21,12 @@
 #include <log4cplus/configurator.h>
 #include <log4cplus/logger.h>
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+
 using namespace std;
 using namespace log4cplus;
 
@@ -145,4 +151,71 @@ archipelago::System::System(const string& logconffile)
             : Logger(logconffile, "System")
 {
     cur_uid = cur_gid = -1;
+}
+
+int archipelago::System::set_system(bool daemonize, int uid, int gid,
+        mode_t mask, const string& pidfile)
+{
+    if (gid != -1) {
+        struct group *gr;
+        gr = getgrgid(gid);
+        if (!gr) {
+            logerror("Cannot find group.");
+            return -1;
+        }
+    }
+
+    if (uid != -1) {
+        struct passwd *pw;
+        pw = getpwuid(uid);
+        if (!pw) {
+            logerror("Cannot find user.");
+            return -1;
+        }
+        username = pw->pw_name;
+        if (gid == -1) {
+            gid = pw->pw_gid;
+        }
+    }
+
+    cur_uid = geteuid();
+    cur_gid = getegid();
+
+    if (gid != -1 && cur_gid != gid && setregid(gid, gid)) {
+        logerror("Could not set process gid.");
+        return -1;
+    }
+
+    if (uid != -1) {
+        if ((cur_gid != gid || cur_uid != uid) &&initgroups(username, gid)) {
+            logerror("Could not initialize groups.");
+            return -1;
+        }
+
+        if (cur_uid != uid && setreuid(uid, uid)) {
+            logerror("Failed to set process uid.");
+            return -1;
+        }
+    }
+
+    mask &= 0777;
+    umask(mask);
+
+    if (close(STDIN_FILENO)) {
+        logwarn("Could not close stdin.");
+    }
+
+    if (daemonize) {
+        loginfo("Becoming daemon.");
+        if (daemon(0, 1) < 0) {
+            logerror("daemon() error.");
+            return -1;
+        }
+        (void) setpgrp();
+    }
+
+    if (write_pid(pidfile) == 0) {
+        return -1;
+    }
+    return 0;
 }
