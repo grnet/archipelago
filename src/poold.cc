e33890bfa70ba2d7e090771f2b3bb45c4ebce037
Chrysostomos Nanakos <cnanakos@grnet.gr>
2014-12-24 11:57:40 +0200

    poold: Add archipelago::Epoll::set_fd_pollin function
    Signed-off-by: Chrysostomos Nanakos <cnanakos@grnet.gr>

--- src/poold.cc
+++ src/poold.cc
@@ -536,3 +536,15 @@ bool archipelago::Epoll::rm_socket(Socket& socket)
     socket.events = 0;
     return true;
 }
+
+bool archipelago::Epoll::set_fd_pollin(int fd, uint32_t events)
+{
+    struct epoll_event ev;
+    ev.data.fd = fd;
+    ev.events = events | EPOLLIN;
+    if (epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &ev) == -1) {
+        perror("epoll_ctl: fd");
+        return false;
+    }
+    return true;
+}
