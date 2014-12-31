d7bce8e56960ddca7daf253df8a172a65b726520
Chrysostomos Nanakos <cnanakos@grnet.gr>
2015-01-01 00:53:37 +0200

    hash: Fix coding style issues
    Signed-off-by: Chrysostomos Nanakos <cnanakos@grnet.gr>

--- src/hash.c
+++ src/hash.c
@@ -59,9 +59,9 @@ static char get_hex(unsigned int h)
 void hexlify(unsigned char *data, long datalen, char *hex)
 {
 	long i;
-	for (i=0; i<datalen; i++){
-		hex[2*i] = get_hex((data[i] & 0xF0) >> 4);
-		hex[2*i + 1] = get_hex(data[i] & 0x0F);
+	for (i = 0; i < datalen; i++) {
+		hex[2 * i] = get_hex((data[i] & 0xF0) >> 4);
+		hex[2 * i + 1] = get_hex(data[i] & 0x0F);
 	}
 }
 
@@ -69,33 +69,29 @@ void unhexlify(char *hex, unsigned char *data)
 {
 	int i;
 	char c;
-	for (i=0; i<SHA256_DIGEST_LENGTH; i++){
+	for (i=0; i<SHA256_DIGEST_LENGTH; i++) {
 		data[i] = 0;
 		c = hex[2*i];
-		if (isxdigit(c)){
-			if (isdigit(c)){
+		if (isxdigit(c)) {
+			if (isdigit(c)) {
 				c-= '0';
-			}
-			else {
+			} else {
 				c = tolower(c);
 				c = c-'a' + 10;
 			}
-		}
-		else {
+		} else {
 			c = 0;
 		}
 		data[i] |= (c << 4) & 0xF0;
 		c = hex[2*i+1];
-		if (isxdigit(c)){
-			if (isdigit(c)){
+		if (isxdigit(c)) {
+			if (isdigit(c)) {
 				c-= '0';
-			}
-			else {
+			} else {
 				c = tolower(c);
 				c = c-'a' + 10;
 			}
-		}
-		else {
+		} else {
 			c = 0;
 		}
 		data[i] |= c & 0x0F;
@@ -110,16 +106,17 @@ void merkle_hash(unsigned char *hashes, unsigned long len,
 	unsigned char *buf;
 	unsigned char tmp_hash[SHA256_DIGEST_SIZE];
 
-	if (!nr){
+	if (!nr) {
 		SHA256(hashes, 0, hash);
 		return;
 	}
-	if (nr == 1){
+	if (nr == 1) {
 		memcpy(hash, hashes, SHA256_DIGEST_SIZE);
 		return;
 	}
-	while (s < nr)
+	while (s < nr) {
 		s = s << 1;
+    }
 	buf = malloc(sizeof(unsigned char)* SHA256_DIGEST_SIZE * s);
 	memcpy(buf, hashes, nr * SHA256_DIGEST_SIZE);
 	memset(buf + nr * SHA256_DIGEST_SIZE, 0, (s - nr) * SHA256_DIGEST_SIZE);
@@ -133,4 +130,3 @@ void merkle_hash(unsigned char *hashes, unsigned long len,
 	}
 	memcpy(hash, buf, SHA256_DIGEST_SIZE);
 }
-
