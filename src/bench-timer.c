55f435d6097cf27386f43aa00ac91d69a54482c7
Chrysostomos Nanakos <cnanakos@grnet.gr>
2015-01-01 16:25:56 +0200

    bench-timer: Change coding style to K&R
    Signed-off-by: Chrysostomos Nanakos <cnanakos@grnet.gr>

--- src/bench-timer.c
+++ src/bench-timer.c
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <bench-xseg.h>
 #include <limits.h>
 
-#define SEC 1000000000 //1sec = 10^9 nsec
+#define SEC 1000000000          //1sec = 10^9 nsec
 #define SEC2 (uint64_t) SEC*SEC //1sec*1sec = 10^18 nsec^2
 
 /*
@@ -37,108 +37,104 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * Subtraction can result to a negative value, so we check for both cases
  */
 static inline void timespecsub(struct timespec *end,
-		struct timespec *start, struct timespec *result)
+                               struct timespec *start, struct timespec *result)
 {
-	if (start->tv_nsec > end->tv_nsec) {
-		result->tv_nsec = SEC - start->tv_nsec + end->tv_nsec;
-		result->tv_sec = end->tv_sec - start->tv_sec - 1;
-	} else {
-		result->tv_nsec = end->tv_nsec - start->tv_nsec;
-		result->tv_sec = end->tv_sec - start->tv_sec;
-	}
+    if (start->tv_nsec > end->tv_nsec) {
+        result->tv_nsec = SEC - start->tv_nsec + end->tv_nsec;
+        result->tv_sec = end->tv_sec - start->tv_sec - 1;
+    } else {
+        result->tv_nsec = end->tv_nsec - start->tv_nsec;
+        result->tv_sec = end->tv_sec - start->tv_sec;
+    }
 }
 
 static inline void timespecadd(struct timespec *a,
-		struct timespec *b, struct timespec *result)
+                               struct timespec *b, struct timespec *result)
 {
-	if (a->tv_nsec + b->tv_nsec >= SEC) {
-		result->tv_nsec = a->tv_nsec + b->tv_nsec - SEC;
-		result->tv_sec = a->tv_sec + b->tv_sec + 1;
-	} else {
-		result->tv_nsec = a->tv_nsec + b->tv_nsec;
-		result->tv_sec = a->tv_sec + b->tv_sec;
-	}
+    if (a->tv_nsec + b->tv_nsec >= SEC) {
+        result->tv_nsec = a->tv_nsec + b->tv_nsec - SEC;
+        result->tv_sec = a->tv_sec + b->tv_sec + 1;
+    } else {
+        result->tv_nsec = a->tv_nsec + b->tv_nsec;
+        result->tv_sec = a->tv_sec + b->tv_sec;
+    }
 }
 
 int init_timer(struct timer **tm, int insanity)
 {
-	*tm = malloc(sizeof(struct timer));
-	if (!*tm) {
-		perror("malloc");
-		return -1;
-	}
+    *tm = malloc(sizeof(struct timer));
+    if (!*tm) {
+        perror("malloc");
+        return -1;
+    }
 
-	memset(*tm, 0, sizeof(struct timer));
-	(*tm)->insanity = insanity;
-	return 0;
+    memset(*tm, 0, sizeof(struct timer));
+    (*tm)->insanity = insanity;
+    return 0;
 }
 
 void timer_start(struct bench *prefs, struct timer *timer)
 {
-	//We need a low-latency way to get current time in nanoseconds.
-	//QUESTION: Is this way the best way?
-	if (GET_FLAG(INSANITY, prefs->flags) < timer->insanity) {
-		return;
+    //We need a low-latency way to get current time in nanoseconds.
+    //QUESTION: Is this way the best way?
+    if (GET_FLAG(INSANITY, prefs->flags) < timer->insanity) {
+        return;
     }
 
-	clock_gettime(CLOCK_BENCH, &timer->start_time);
+    clock_gettime(CLOCK_BENCH, &timer->start_time);
 }
 
 void timer_stop(struct bench *prefs, struct timer *timer,
-		struct timespec *start_time)
+                struct timespec *start_time)
 {
-	struct timespec end_time;
+    struct timespec end_time;
 
-	if (GET_FLAG(INSANITY, prefs->flags) < timer->insanity) {
-		return;
+    if (GET_FLAG(INSANITY, prefs->flags) < timer->insanity) {
+        return;
     }
 
-	/*
-	 * There are timers such as rec_tm whose start_time cannot be trusted
-	 * and the submission time is stored in other structs (e.g. struct
-	 * peer_request).
-	 * In this case, the submission time must be passed explicitly to this
-	 * function using the "start" argument.
-	 */
-	if (!start_time) {
-		start_time = &timer->start_time;
+    /*
+     * There are timers such as rec_tm whose start_time cannot be trusted
+     * and the submission time is stored in other structs (e.g. struct
+     * peer_request).
+     * In this case, the submission time must be passed explicitly to this
+     * function using the "start" argument.
+     */
+    if (!start_time) {
+        start_time = &timer->start_time;
     }
 
-	clock_gettime(CLOCK_BENCH, &end_time);
+    clock_gettime(CLOCK_BENCH, &end_time);
 
-	timespecsub(&end_time, start_time, &timer->elapsed_time);
+    timespecsub(&end_time, start_time, &timer->elapsed_time);
 
-	//Add the elapsed time to the current sum for this timer.
-	//For accuracy, nanoseconds' sum has to be always less that 10^9
-	timespecadd(&timer->elapsed_time, &timer->sum, &timer->sum);
+    //Add the elapsed time to the current sum for this timer.
+    //For accuracy, nanoseconds' sum has to be always less that 10^9
+    timespecadd(&timer->elapsed_time, &timer->sum, &timer->sum);
 
 #if 0
-	struct timespec2 elapsed_time_sq;
+    struct timespec2 elapsed_time_sq;
 
-	//Add elapsed_time^2 to the current sum of squares for this timer
-	//This is needed to calculate standard deviation.
-	//As above, the sum of square of nanoseconds has to be less than 10^18
-	elapsed_time_sq.tv_sec2 = elapsed_time.tv_sec*elapsed_time.tv_sec;
-	elapsed_time_sq.tv_nsec2 = elapsed_time.tv_nsec*elapsed_time.tv_nsec;
-	if (elapsed_time_sq.tv_nsec2 + timer->sum_sq.tv_nsec2 > SEC2) {
-		timer->sum_sq.tv_nsec2 =
-			(timer->sum_sq.tv_nsec2 + elapsed_time_sq.tv_nsec2) % SEC2;
-		timer->sum_sq.tv_sec2 += elapsed_time_sq.tv_sec2 + 1;
-	} else {
-		timer->sum_sq.tv_nsec2 += elapsed_time_sq.tv_nsec2;
-		timer->sum_sq.tv_sec2 += elapsed_time_sq.tv_sec2;
-	}
+    //Add elapsed_time^2 to the current sum of squares for this timer
+    //This is needed to calculate standard deviation.
+    //As above, the sum of square of nanoseconds has to be less than 10^18
+    elapsed_time_sq.tv_sec2 = elapsed_time.tv_sec * elapsed_time.tv_sec;
+    elapsed_time_sq.tv_nsec2 = elapsed_time.tv_nsec * elapsed_time.tv_nsec;
+    if (elapsed_time_sq.tv_nsec2 + timer->sum_sq.tv_nsec2 > SEC2) {
+        timer->sum_sq.tv_nsec2 =
+            (timer->sum_sq.tv_nsec2 + elapsed_time_sq.tv_nsec2) % SEC2;
+        timer->sum_sq.tv_sec2 += elapsed_time_sq.tv_sec2 + 1;
+    } else {
+        timer->sum_sq.tv_nsec2 += elapsed_time_sq.tv_nsec2;
+        timer->sum_sq.tv_sec2 += elapsed_time_sq.tv_sec2;
+    }
 #endif
-	//TODO: check if we need to make it volatile
-	timer->completed++;
+    //TODO: check if we need to make it volatile
+    timer->completed++;
 
-	/*
-	printf("Start: %lu s %lu ns\n", start_time.tv_sec, start_time.tv_nsec);
-	printf("Elpsd: %lu s %lu ns\n", elapsed_time.tv_sec, elapsed_time.tv_nsec);
-	printf("End:   %lu s %lu ns\n", end_time.tv_sec, end_time.tv_nsec);
-	*/
+    /*
+       printf("Start: %lu s %lu ns\n", start_time.tv_sec, start_time.tv_nsec);
+       printf("Elpsd: %lu s %lu ns\n", elapsed_time.tv_sec, elapsed_time.tv_nsec);
+       printf("End:   %lu s %lu ns\n", end_time.tv_sec, end_time.tv_nsec);
+     */
 }
-
-
-
-
