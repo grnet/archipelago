ddc12bde24a8c6f54128478c503057a881389aee
Chrysostomos Nanakos <cnanakos@grnet.gr>
2015-01-01 16:10:34 +0200

    bench-utils: Fix coding style issues
    Signed-off-by: Chrysostomos Nanakos <cnanakos@grnet.gr>

--- src/bench-utils.c
+++ src/bench-utils.c
@@ -36,8 +36,9 @@ struct timespec delay = {0, 4000000};
 
 uint64_t __get_object(struct bench *prefs, uint64_t new)
 {
-	if (prefs->ts > 0)
+	if (prefs->ts > 0) {
 		new = new / (prefs->os / prefs->bs);
+    }
 	return new;
 }
 
@@ -56,10 +57,11 @@ uint64_t str2num(char *str)
 	uint64_t num;
 
 	num = strtoll(str, &unit, 10);
-	if (strlen(unit) > 1) //Invalid syntax
+	if (strlen(unit) > 1) {//Invalid syntax
 		return 0;
-	else if (strlen(unit) < 1) //Plain number in bytes
+    } else if (strlen(unit) < 1) {//Plain number in bytes
 		return num;
+    }
 
 	switch (*unit) {
 		case 'g':
@@ -83,58 +85,74 @@ uint64_t str2num(char *str)
  */
 int read_insanity(char *insanity)
 {
-	if (strncmp(insanity, "sane", MAX_ARG_LEN + 1) == 0)
+	if (strncmp(insanity, "sane", MAX_ARG_LEN + 1) == 0) {
 		return INSANITY_SANE;
-	if (strncmp(insanity, "eccentric", MAX_ARG_LEN + 1) == 0)
+    }
+	if (strncmp(insanity, "eccentric", MAX_ARG_LEN + 1) == 0) {
 		return INSANITY_ECCENTRIC;
-	if (strncmp(insanity, "manic", MAX_ARG_LEN + 1) == 0)
+    }
+	if (strncmp(insanity, "manic", MAX_ARG_LEN + 1) == 0) {
 		return INSANITY_MANIC;
-	if (strncmp(insanity, "paranoid", MAX_ARG_LEN + 1) == 0)
+    }
+	if (strncmp(insanity, "paranoid", MAX_ARG_LEN + 1) == 0) {
 		return INSANITY_PARANOID;
+    }
 	return -1;
 }
 
 int read_op(char *op)
 {
-	if (strncmp(op, "read", MAX_ARG_LEN + 1) == 0)
+	if (strncmp(op, "read", MAX_ARG_LEN + 1) == 0) {
 		return X_READ;
-	if (strncmp(op, "write", MAX_ARG_LEN + 1) == 0)
+    }
+	if (strncmp(op, "write", MAX_ARG_LEN + 1) == 0) {
 		return X_WRITE;
-	if (strncmp(op, "info", MAX_ARG_LEN + 1) == 0)
+    }
+	if (strncmp(op, "info", MAX_ARG_LEN + 1) == 0) {
 		return X_INFO;
-	if (strncmp(op, "delete", MAX_ARG_LEN + 1) == 0)
+    }
+	if (strncmp(op, "delete", MAX_ARG_LEN + 1) == 0) {
 		return X_DELETE;
+    }
 	return -1;
 }
 
 int read_verify(char *verify)
 {
-	if (strncmp(verify, "no", MAX_ARG_LEN + 1) == 0)
+	if (strncmp(verify, "no", MAX_ARG_LEN + 1) == 0) {
 		return VERIFY_NO;
-	if (strncmp(verify, "meta", MAX_ARG_LEN + 1) == 0)
+    }
+	if (strncmp(verify, "meta", MAX_ARG_LEN + 1) == 0) {
 		return VERIFY_META;
-	if (strncmp(verify, "full", MAX_ARG_LEN + 1) == 0)
+    }
+	if (strncmp(verify, "full", MAX_ARG_LEN + 1) == 0) {
 		return VERIFY_FULL;
+    }
 	return -1;
 }
 
 int read_progress(char *progress)
 {
-	if (strncmp(progress, "no", MAX_ARG_LEN + 1) == 0)
+	if (strncmp(progress, "no", MAX_ARG_LEN + 1) == 0) {
 		return PROGRESS_NO;
-	if (strncmp(progress, "yes", MAX_ARG_LEN + 1) == 0)
+    }
+	if (strncmp(progress, "yes", MAX_ARG_LEN + 1) == 0) {
 		return PROGRESS_YES;
+    }
 	return -1;
 }
 
 int read_progress_type(char *ptype)
 {
-	if (strncmp(ptype, "req", MAX_ARG_LEN + 1) == 0)
+	if (strncmp(ptype, "req", MAX_ARG_LEN + 1) == 0) {
 		return PTYPE_REQ;
-	if (strncmp(ptype, "io", MAX_ARG_LEN + 1) == 0)
+    }
+	if (strncmp(ptype, "io", MAX_ARG_LEN + 1) == 0) {
 		return PTYPE_IO;
-	if (strncmp(ptype, "both", MAX_ARG_LEN + 1) == 0)
+    }
+	if (strncmp(ptype, "both", MAX_ARG_LEN + 1) == 0) {
 		return PTYPE_BOTH;
+    }
 	return -1;
 }
 
@@ -150,39 +168,46 @@ uint64_t read_interval(struct bench *prefs, char *str_interval)
 	interval = strtoll(str_interval, &unit, 10);
 
 	/* If interval is raw number of requests */
-	if (!unit[0] && interval < prefs->status->max)
+	if (!unit[0] && interval < prefs->status->max) {
 		return interval;
+    }
 
 	/* If interval is percentage of max requests */
 	if (strncmp(unit, "%", MAX_ARG_LEN + 1) == 0 &&
-			interval > 0 && interval < 100)
+			interval > 0 && interval < 100) {
 		return calculate_interval(prefs, interval);
+    }
 
 	return 0;
 }
 
 int read_ping(char *ping)
 {
-	if (strncmp(ping, "no", MAX_ARG_LEN + 1) == 0)
+	if (strncmp(ping, "no", MAX_ARG_LEN + 1) == 0) {
 		return PING_MODE_OFF;
-	if (strncmp(ping, "yes", MAX_ARG_LEN + 1) == 0)
+    }
+	if (strncmp(ping, "yes", MAX_ARG_LEN + 1) == 0) {
 		return PING_MODE_ON;
+    }
 	return -1;
 }
 
 int read_pattern(char *pattern)
 {
-	if (strncmp(pattern, "seq", MAX_ARG_LEN + 1) == 0)
+	if (strncmp(pattern, "seq", MAX_ARG_LEN + 1) == 0) {
 		return PATTERN_SEQ;
-	if (strncmp(pattern, "rand", MAX_ARG_LEN + 1) == 0)
+    }
+	if (strncmp(pattern, "rand", MAX_ARG_LEN + 1) == 0) {
 		return PATTERN_RAND;
+    }
 	return -1;
 }
 
 int validate_seed(struct bench *prefs, unsigned long seed)
 {
-	if (seed < pow(10, prefs->objvars->seedlen))
+	if (seed < pow(10, prefs->objvars->seedlen)) {
 		return 0;
+    }
 	return -1;
 }
 
@@ -191,8 +216,9 @@ uint64_t calculate_interval(struct bench *prefs, uint64_t percentage)
 	uint64_t interval = round((double)prefs->status->max *
 			(double)percentage / 100);
 
-	if (!interval)
+	if (!interval) {
 		interval = 1;
+    }
 
 	return interval;
 }
@@ -228,17 +254,18 @@ void create_target(struct bench *prefs, struct xseg_request *req)
 
 uint64_t determine_next(struct bench *prefs)
 {
-	if (GET_FLAG(PATTERN, prefs->flags) == PATTERN_SEQ)
+	if (GET_FLAG(PATTERN, prefs->flags) == PATTERN_SEQ) {
 		return prefs->status->submitted;
-	else
+    } else {
 		return lfsr_next(prefs->lfsr);
+    }
 }
 
 uint64_t calculate_offset(struct bench *prefs, uint64_t new)
 {
-	if (prefs->ts > 0)
+	if (prefs->ts > 0) {
 		return (new * prefs->bs) % prefs->os;
-	else
+    } else {
 		return 0;
+    }
 }
-
